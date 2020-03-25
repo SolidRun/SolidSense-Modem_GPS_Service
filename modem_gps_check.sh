a7cdafd4fa361404b1e03fa6016c0f3b9996b150
Eric Bree <eric@merkbree.com>
2020-03-25 14:29:09 -0400

    modem_gps_check: fix logic
     * update MAX_COUNT to 200
     * fix logix for checking for MODEM_OK

--- modem_gps_check.sh
+++ modem_gps_check.sh
@@ -2,10 +2,11 @@
 
 count=0
 SLEEP=0.1
-MAX_COUNT=60
+MAX_COUNT=200
 
 while (/bin/true) ; do
-	if journalctl -xab -u modem_gps.service -p crit | grep MODEM_OK ; then
+	val=$(journalctl -xab -u modem_gps.service -p crit | grep MODEM_OK)
+	if [ -n "${val}" ]; then
 		break
 	else
 		sleep "${SLEEP}"
@@ -14,6 +15,6 @@ while (/bin/true) ; do
 	if [ "${count}" -gt "${MAX_COUNT}" ] ; then
 		slept=$(echo "${SLEEP} * ${MAX_COUNT}" | bc)
 		logger -i --priority crit --tag modem_gps_check "MAX_COUNT:SLEEP:TIME <${MAX_COUNT}:${SLEEP}:${slept}s> exceeded!"
-		break;
+		break
 	fi
 done
